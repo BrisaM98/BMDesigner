f2d647bc773850544228695e08503876e7cf6704
BrisaM98 <al179706@alumnos.uacj.mx>
2024-11-11 14:52:26 -0700

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-// Obtener el botón y la ventana emergente
-const acceptButton = document.getElementById('acceptButton');
+// Obtener el popup y el botón de aceptación
 const cookiePopup = document.getElementById('cookiePopup');
+const acceptButton = document.getElementById('acceptButton');
 
-// Al hacer clic en el botón de aceptar, se oculta la ventana emergente
+// Comprobar si el usuario ya ha aceptado las cookies
+if (!localStorage.getItem('cookiesAccepted')) {
+    // Si no se ha almacenado la aceptación, mostrar el popup
+    cookiePopup.style.display = 'flex';
+}
+
+// Manejar el evento de clic en el botón de aceptar
 acceptButton.addEventListener('click', function() {
-    cookiePopup.classList.add('hidden'); // Agrega la clase "hidden" para ocultar el popup
+    // Al hacer clic en Aceptar, ocultar el popup
+    cookiePopup.style.display = 'none';
+    
+    // Guardar que el usuario aceptó las cookies en localStorage
+    localStorage.setItem('cookiesAccepted', 'true');
 });
